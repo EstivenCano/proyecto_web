1a87a545dc0d932038c02d3c5d31cfa9ffd0acdf
JoanCadena <inmortal_20@live.com>
2020-05-20 13:09:38 -0500

    Corrección de errores

--- Frontend/pages/listaConvenios.vue
+++ Frontend/pages/listaConvenios.vue
@@ -25,12 +25,7 @@
 
       <b-form action="javascript:void(0)" @submit="cargarLSpais()" @reset="onReset" id="f2">
         <b-form-group id="pais" label="Filtrar por País:" label-for="filtro2">
-          <b-form-select
-            id="id_pais"
-            v-model="pais.id"
-            :options="paises"
-            required
-          ></b-form-select>
+          <b-form-select id="id_pais" v-model="pais.id" :options="paises" required></b-form-select>
         </b-form-group>
 
         <center>
@@ -41,12 +36,7 @@
 
       <b-form action="javascript:void(0)" @submit="cargarLSentidad()" @reset="onReset" id="f3">
         <b-form-group id="entidad" label="Filtrar por Entidad:" label-for="filtro3">
-          <b-form-select
-            id="id_entidad"
-            v-model="entidad.id"
-            :options="entidades"
-            required
-          ></b-form-select>
+          <b-form-select id="id_entidad" v-model="entidad.id" :options="entidades" required></b-form-select>
         </b-form-group>
 
         <center>
@@ -57,24 +47,36 @@
 
       <b-table striped hover :items="lista_convenios" :fields="fields1" v-if="cargada1">
         <template v-slot:cell(acciones)="row">
-          <b-button size="sm" @click="cargarConvenio(row)" class="mr-2">Divulgar</b-button>
+          <b-button size="sm" @click="lsDatosCorreo(row)" class="mr-2">Divulgar</b-button>
         </template>
       </b-table>
 
       <b-table striped hover :items="lista_convenios" :fields="fields2" v-if="cargada2">
         <template v-slot:cell(acciones)="row">
-          <b-button size="sm" @click="cargarConvenio(row)" class="mr-2">Divulgar</b-button>
+          <b-button size="sm" @click="lsDatosCorreo(row)" class="mr-2">Divulgar</b-button>
         </template>
       </b-table>
 
       <b-table striped hover :items="lista_convenios" :fields="fields3" v-if="cargada3">
         <template v-slot:cell(acciones)="row">
-          <b-button size="sm" @click="lsDatosCorreo(row)" class="mr-2"
-            >Divulgar</b-button
-          >
+          <b-button size="sm" @click="lsDatosCorreo(row)" class="mr-2">Divulgar</b-button>
         </template>
       </b-table>
       <br />
+      <b-modal ref="my-modal" hide-footer title="Divulgación de convenio">
+        <div class="d-block text-center">
+          <b-form-group
+            id="input-group-1"
+            label="Correos Electrónicos:"
+            label-for="input-1"
+            description="Separados por ',' (example@gmail.com,another@gmail.com)"
+          >
+            <b-form-input id="correos" required placeholder="Ingresa los correos"></b-form-input>
+          </b-form-group>
+        </div>
+        <b-button class="mt-2" variant="outline-success" block @click="enviarCorreo">Enviar</b-button>
+        <b-button class="mt-3" variant="outline-danger" block @click="ocultarModal">Cerrar</b-button>
+      </b-modal>
     </b-container>
   </div>
 </template>
